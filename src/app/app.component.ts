04d2f1a9b4799635d9f632ea22694e3060d47cbc
merlin <tamlamerlin@gmail.com>
2022-09-26 20:40:17 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'skeleton-app';
+
+  loaded = false;
+
+  ngOnInit(): void {
+    setInterval(() => {
+      this.loaded = true;
+    }, 3000);
+  }
 }
---
+++ src/app/skeleton-loader/skeleton-loader.component.ts
@@ -0,0 +1,30 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-skeleton-loader',
+  template: `
+  <div [ngStyle]="getMyStyles()" class="skelt-load loader"></div>
+  `,
+  styleUrls: ['./skeleton-loader.component.scss']
+})
+export class SkeletonLoaderComponent implements OnInit {
+
+  @Input() Cwidth: any;
+  @Input() Cheight: any;
+  @Input() circle: boolean = false;
+
+  constructor() { }
+
+  getMyStyles() {
+    const myStyles = {
+      'width.px': this.Cwidth ? this.Cwidth : '',
+      'height.px': this.Cheight ? this.Cheight : '',
+      'border-radius': this.circle ? '50%' : ''
+    };
+    return myStyles;
+  }
+
+  ngOnInit(): void {
+  }
+
+}
